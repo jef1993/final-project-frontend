98666a64813c3ad17bb401c2114925971dd03d43
MostlyMagical <jb.mccann@outlook.com>
2021-10-04 16:17:54 +0100

    created basic structure of the home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import "./App.css";
+import react from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 
-function App() {
-  return <div className="App">App</div>;
+import "./App.css";
+import Footer from "./components/Footer"
+
+class App extends react.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      Title: "Netpix",
+      headerLinks: [
+        {Title: "home", paths: "/"},
+        {Title: "Sign In", paths: "/Sign-In"},
+        {Title: "My Lists", paths: "/My-Lists"}
+      ],
+      home: {
+        Title: ""
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p=0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Netpix</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="Sign-In">Sign In</Link>
+                <Link className="nav-link" to="My-Lists">My Lists</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Footer/>
+          
+        </Container>
+      </Router>
+    )
+  }
 }
+
+// function App() {
+//   return <div className="App">App</div>;
+// }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import react from "react"
+import {Container,Row,Col} from "react-bootstrap"
+
+function Footer() {
+    return(
+        <footer className="mt-5">
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
+                    <Col className="p-0" md={3} sm={12}>
+                        Team Extreme
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
