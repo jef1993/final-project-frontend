cd549605a4e8c64865ce59667bcf9b7a5c7a42bc
jef1993 <jeffrey24493@gmail.com>
2021-10-09 00:12:20 +0100

    banner css done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import react from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+// import Container from "react-bootstrap/Container";
+// import Navbar from "react-bootstrap/Navbar";
+// import Nav from "react-bootstrap/Nav";
 
 import "./App.css";
 import Footer from "./components/Footer";
@@ -12,114 +12,123 @@ import ListsPage from "./pages/ListsPage";
 
 import { Banner2 } from "./components/Banner";
 
-class App extends react.Component {
-  // componentDidMount(
-  //   getMovies = async () => {
-  //     const response = await fetch(
-  //       "https://api.themoviedb.org/3/movie/550?api_key=b7cb96b2c3e86cd23a777c56ffafe65c"
-  //     )
-  //     const data = await response.json()
-  //     console.log(data)
-  //   }
-  // )
+// class App extends react.Component {
+// componentDidMount(
+//   getMovies = async () => {
+//     const response = await fetch(
+//       "https://api.themoviedb.org/3/movie/550?api_key=b7cb96b2c3e86cd23a777c56ffafe65c"
+//     )
+//     const data = await response.json()
+//     console.log(data)
+//   }
+// )
 
-  componentDidMount() {
-    fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=b7cb96b2c3e86cd23a777c56ffafe65c&language=en-US&page=1"
-    )
-      .then((response) => response.json())
-      .then((data) =>
-        this.setState({
-          data: data.results.map((x) => ({ ...x, selected: false })),
-        })
-      );
-  }
+//   componentDidMount() {
+//     fetch(
+//       "https://api.themoviedb.org/3/movie/now_playing?api_key=b7cb96b2c3e86cd23a777c56ffafe65c&language=en-US&page=1"
+//     )
+//       .then((response) => response.json())
+//       .then((data) =>
+//         this.setState({
+//           data: data.results.map((x) => ({ ...x, selected: false })),
+//         })
+//       );
+//   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: [],
 
-      Title: "Netpix",
-      headerLinks: [
-        { title: "Home", paths: "/" },
-        { title: "Sign-In", paths: "/Sign-In" },
-        { title: "My-Lists", paths: "/My-Lists" },
-      ],
-      home: {
-        title: "Netpix!",
-        subTitle: "What to watch next, made easy!",
-        text: "Create a custom list from thousands of movies and let us choose what you watch next!",
-      },
-      signIn: {
-        title: "Netpix!",
-        subTitle: "What to watch next, made easy!",
-        text: "Create a custom list from thousands of movies and let us choose what you watch next!",
-      },
-    };
-  }
+//       Title: "Netpix",
+//       headerLinks: [
+//         { title: "Home", paths: "/" },
+//         { title: "Sign-In", paths: "/Sign-In" },
+//         { title: "My-Lists", paths: "/My-Lists" },
+//       ],
+//       home: {
+//         title: "Netpix!",
+//         subTitle: "What to watch next, made easy!",
+//         text: "Create a custom list from thousands of movies and let us choose what you watch next!",
+//       },
+//       signIn: {
+//         title: "Netpix!",
+//         subTitle: "What to watch next, made easy!",
+//         text: "Create a custom list from thousands of movies and let us choose what you watch next!",
+//       },
+//     };
+//   }
 
-  render() {
-    return (
-      <Router>
-        <Container className="p=0" fluid={true}>
-          <Navbar className="border-bottom" bg="transparent" expand="lg">
-            <Navbar.Brand>Netpix</Navbar.Brand>
+//   render() {
+//     return (
+//       <Router>
+//         <Container className="p=0" fluid={true}>
+//           <Navbar className="border-bottom" bg="transparent" expand="lg">
+//             <Navbar.Brand>Netpix</Navbar.Brand>
 
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto">
-                <Link className="nav-link" to="Home">
-                  Home
-                </Link>
-                <Link className="nav-link" to="/">
-                  Sign In
-                </Link>
-                <Link className="nav-link" to="My-Lists">
-                  My Lists
-                </Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+//             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+//             <Navbar.Collapse id="navbar-toggle">
+//               <Nav className="ml-auto">
+//                 <Link className="nav-link" to="Home">
+//                   Home
+//                 </Link>
+//                 <Link className="nav-link" to="/">
+//                   Sign In
+//                 </Link>
+//                 <Link className="nav-link" to="My-Lists">
+//                   My Lists
+//                 </Link>
+//               </Nav>
+//             </Navbar.Collapse>
+//           </Navbar>
 
-          <Route
-            path="/Home"
-            render={() => (
-              <HomePage
-                title={this.state.home.title}
-                subTitle={this.state.home.subTitle}
-                text={this.state.home.text}
-                data={this.state.data}
-              />
-            )}
-          />
-          <Route
-            path="/"
-            exact
-            render={() => (
-              <SignInPage
-                title={this.state.home.title}
-                subTitle={this.state.home.subTitle}
-                text={this.state.home.text}
-              />
-            )}
-          />
-          <Route path="/My-Lists" render={() => <ListsPage />} />
-          <Footer />
-        </Container>
-      </Router>
-    );
-  }
-}
+//           <Route
+//             path="/Home"
+//             render={() => (
+//               <HomePage
+//                 title={this.state.home.title}
+//                 subTitle={this.state.home.subTitle}
+//                 text={this.state.home.text}
+//                 data={this.state.data}
+//               />
+//             )}
+//           />
+//           <Route
+//             path="/"
+//             exact
+//             render={() => (
+//               <SignInPage
+//                 title={this.state.home.title}
+//                 subTitle={this.state.home.subTitle}
+//                 text={this.state.home.text}
+//               />
+//             )}
+//           />
+//           <Route path="/My-Lists" render={() => <ListsPage />} />
+//           <Footer />
+//         </Container>
+//       </Router>
+//     );
+//   }
+// }
 
 export function App2() {
+  const [curUser, setCurUser] = useState();
   return (
     <div className="App">
       <div className="main">
-        <Banner2 />
+        <Router>
+          <Banner2
+            bgImg="bg-1"
+            iconName="login-variant"
+            onClick={() => {
+              console.log("loggin in");
+            }}
+          />
+        </Router>
       </div>
     </div>
   );
 }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App, { App2 } from "./App";
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App2 />, document.getElementById("root"));
