efc95ac46f2bd4cb0d57fb4a7c0c890aaa87f7ef
YungGooby <liam_toole@hotmail.co.uk>
2021-10-12 14:53:34 +0100

    login functionality
    Needs to de-load window on SignIn

--- src/App.js
+++ src/App.js
@@ -1,113 +1,97 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import {
-  trendingMovies,
-  nowPlaying,
-  topRated,
-  upComingMovies,
-} from "./utils/list";
+	trendingMovies,
+	nowPlaying,
+	topRated,
+	upComingMovies,
+} from './utils/list';
 
-import "./App.css";
+import './App.css';
 
-import { Banner } from "./components/Banner";
-import { Overlay } from "./components/Overlay";
-import { List } from "./components/List";
-import { DetailsBottom } from "./components/Details";
-import { toggleOverlay } from "./functions";
+import { Banner } from './components/Banner';
+import { Overlay } from './components/Overlay';
+import { List } from './components/List';
+import { DetailsBottom } from './components/Details';
+import { toggleOverlay } from './functions';
 
 export function App() {
-  const [curUser, setCurUser] = useState("");
+	const [curUser, setCurUser] = useState('');
 
-  const navSwitch = () => {
-    const obj = {};
-    if (curUser === "") {
-      obj.iconName = "login-variant";
-      obj.path = "/";
-    } else {
-      obj.iconName = "account-box-outline";
-      obj.path = `/account`;
-    }
-    return obj;
-  };
+	const navSwitch = () => {
+		const obj = {};
+		if (curUser === '') {
+			obj.iconName = 'login-variant';
+			obj.path = '/';
+		} else {
+			obj.iconName = 'account-box-outline';
+			obj.path = `/account`;
+		}
+		return obj;
+	};
 
-  const userHandler = () => {
-    if (curUser === "") {
-      toggleOverlay();
-    }
-  };
-  const loginHandler = () => {};
+	const userHandler = () => {
+		if (curUser === '') {
+			toggleOverlay();
+		}
+	};
 
-  const registerHandler = () => {};
+	const logoutHandler = () => {
+		setCurUser('');
+	};
 
-  const logoutHandler = () => {
-    setCurUser("");
-  };
-
-  return (
-    <Router>
-      <div className="App">
-        <Overlay
-          onSignIn={loginHandler}
-          onRegister={registerHandler}
-          usernameChange={(e) => {
-            console.log(`username is ${e}!`);
-          }}
-          passwordChange={(e) => {
-            console.log(`password is ${e}!`);
-          }}
-          emailChange={(e) => {
-            console.log(`email is ${e}!`);
-          }}
-        />
-
-        <div className="main">
-          <Switch>
-            <Route exact path="/">
-              <Banner
-                bgImg="bg-1"
-                iconName={navSwitch().iconName}
-                linkTo={navSwitch().path}
-                userNameTop={curUser}
-                navBtn={userHandler}
-              />
-            </Route>
-            <Route path="/account">
-              <Banner
-                bgImg="bg-2"
-                iconName="backspace-outline"
-                userName={curUser}
-                resetUser={() => {
-                  logoutHandler();
-                }}
-                linkTo="/"
-              />
-            </Route>
-            <Route path="/movies">
-              <Banner
-                bgImg="banner__backdrop"
-                iconName="backspace-outline"
-                userName={curUser}
-                resetUser={() => {
-                  logoutHandler();
-                }}
-                linkTo="/"
-              />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route exact path="/">
-              <List fetchFunc={trendingMovies} title="Trending" />
-              <List fetchFunc={nowPlaying} title="Now in Cinema" />
-              <List fetchFunc={topRated} title="UK's favourite" />
-              <List fetchFunc={upComingMovies} title="upcoming" />
-            </Route>
-            <Route path="/movies">
-              <DetailsBottom />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Overlay setCurUser={setCurUser} />
+				<div className='main'>
+					<Switch>
+						<Route exact path='/'>
+							<Banner
+								bgImg='bg-1'
+								iconName={navSwitch().iconName}
+								linkTo={navSwitch().path}
+								userNameTop={curUser}
+								navBtn={userHandler}
+							/>
+						</Route>
+						<Route path='/account'>
+							<Banner
+								bgImg='bg-2'
+								iconName='backspace-outline'
+								userName={curUser}
+								resetUser={() => {
+									logoutHandler();
+								}}
+								linkTo='/'
+							/>
+						</Route>
+						<Route path='/movies'>
+							<Banner
+								bgImg='banner__backdrop'
+								iconName='backspace-outline'
+								userName={curUser}
+								resetUser={() => {
+									logoutHandler();
+								}}
+								linkTo='/'
+							/>
+						</Route>
+					</Switch>
+					<Switch>
+						<Route exact path='/'>
+							<List fetchFunc={trendingMovies} title='Trending' />
+							<List fetchFunc={nowPlaying} title='Now in Cinema' />
+							<List fetchFunc={topRated} title="UK's favourite" />
+							<List fetchFunc={upComingMovies} title='upcoming' />
+						</Route>
+						<Route path='/movies'>
+							<DetailsBottom />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,104 +1,118 @@
-import React from "react";
+import React, { useState } from 'react';
 // import { useHistory } from "react-router-dom";
-import { toggleClass, toggleClassAll } from "../functions";
+import { toggleClass, toggleClassAll } from '../functions';
+import { logIn } from '../utils';
 
-export const Overlay = (props) => {
-  //   const history = useHistory();
-  //   const homeBtn = (path) => history.push(`${path}`);
+export const Overlay = ({ setCurUser }) => {
+	//   const history = useHistory();
+	//   const homeBtn = (path) => history.push(`${path}`);
+	const [usernameChange, setUsernameChange] = useState('');
+	const [passwordChange, setPasswordChange] = useState('');
+	const [emailChange, setEmailChange] = useState('');
 
-  return (
-    <div className="overlay hidden">
-      <div
-        className="overlay__bg"
-        onClick={() => {
-          toggleClass("overlay", "hidden");
-        }}
-      ></div>
-      <div className="overlay__container">
-        <div className="overlay__title">
-          <h2 className="logo overlay__icon">
-            MOVILIST<span>Account</span>
-          </h2>
-        </div>
-        <div className="overlay__content overlay__signin">
-          <form className="overlay__form" onSubmit={props.onSignIn}>
-            <div className="overlay__inputs">
-              <input
-                className="overlay__input"
-                type="text"
-                placeholder="Username"
-                onChange={(e) => {
-                  props.usernameChange(e.target.value);
-                }}
-              ></input>
-              <input
-                className="overlay__input"
-                type="password"
-                placeholder="Password"
-                onChange={(e) => {
-                  props.passwordChange(e.target.value);
-                }}
-              ></input>
-            </div>
+	const loginHandler = (e) => {
+		e.preventDefault();
+		logIn(emailChange, passwordChange, setCurUser);
+	};
 
-            <button className="overlay__btn">Sign In</button>
-          </form>
+	const registerHandler = (e) => {
+    e.preventDefault();
 
-          <p className="overlay__link">
-            New user?{" "}
-            <span
-              onClick={() => {
-                toggleClassAll("overlay__content", "swapToRight");
-              }}
-            >
-              Create an account
-            </span>
-          </p>
-        </div>
-        <div className="overlay__content overlay__signup">
-          <form className="overlay__form" onSubmit={props.onRegister}>
-            <div className="overlay__inputs">
-              <input
-                className="overlay__input"
-                type="text"
-                placeholder="Username"
-                onChange={(e) => {
-                  props.usernameChange(e.target.value);
-                }}
-              ></input>
-              <input
-                className="overlay__input"
-                type="email"
-                placeholder="email"
-                onChange={(e) => {
-                  props.emailChange(e.target.value);
-                }}
-              ></input>
-              <input
-                className="overlay__input"
-                type="password"
-                placeholder="Password"
-                onChange={(e) => {
-                  props.passwordChange(e.target.value);
-                }}
-              ></input>
-            </div>
+  };
 
-            <button className="overlay__btn">Register</button>
-          </form>
+	return (
+		<div className='overlay hidden'>
+			<div
+				className='overlay__bg'
+				onClick={() => {
+					toggleClass('overlay', 'hidden');
+				}}
+			></div>
+			<div className='overlay__container'>
+				<div className='overlay__title'>
+					<h2 className='logo overlay__icon'>
+						MOVILIST<span>Account</span>
+					</h2>
+				</div>
+				<div className='overlay__content overlay__signin'>
+					<form className='overlay__form' onSubmit={loginHandler}>
+						<div className='overlay__inputs'>
+							<input
+								className='overlay__input'
+								type='text'
+								placeholder='Email'
+								onChange={(e) => {
+									setEmailChange(e.target.value);
+								}}
+							></input>
+							<input
+								className='overlay__input'
+								type='password'
+								placeholder='Password'
+								onChange={(e) => {
+									setPasswordChange(e.target.value);
+								}}
+							></input>
+						</div>
 
-          <p className="overlay__link">
-            Have a account?{" "}
-            <span
-              onClick={() => {
-                toggleClassAll("overlay__content", "swapToRight");
-              }}
-            >
-              Sign In
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+						<button className='overlay__btn'>Sign In</button>
+					</form>
+
+					<p className='overlay__link'>
+						New user?{' '}
+						<span
+							onClick={() => {
+								toggleClassAll('overlay__content', 'swapToRight');
+							}}
+						>
+							Create an account
+						</span>
+					</p>
+				</div>
+				<div className='overlay__content overlay__signup'>
+					<form className='overlay__form' onSubmit={registerHandler}>
+						<div className='overlay__inputs'>
+							<input
+								className='overlay__input'
+								type='text'
+								placeholder='Username'
+								onChange={(e) => {
+									setUsernameChange(e.target.value);
+								}}
+							></input>
+							<input
+								className='overlay__input'
+								type='email'
+								placeholder='email'
+								onChange={(e) => {
+									setEmailChange(e.target.value);
+								}}
+							></input>
+							<input
+								className='overlay__input'
+								type='password'
+								placeholder='Password'
+								onChange={(e) => {
+									setPasswordChange(e.target.value);
+								}}
+							></input>
+						</div>
+
+						<button className='overlay__btn'>Register</button>
+					</form>
+
+					<p className='overlay__link'>
+						Have a account?{' '}
+						<span
+							onClick={() => {
+								toggleClassAll('overlay__content', 'swapToRight');
+							}}
+						>
+							Sign In
+						</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
